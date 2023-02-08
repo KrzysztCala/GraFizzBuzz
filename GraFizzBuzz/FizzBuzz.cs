c020361bb17c122b6cbd106d1b8b34ea7fd919f4
KrzysztofCała <k.cala@parsifal.com.pl>
2023-02-08 18:02:11 +0100

    Add project files.

---
+++ GraFizzBuzz/FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraFizzBuzz
+{
+    internal class FizzBuzz
+    {
+        public FizzBuzz(int number)
+        {
+            Number = number;
+        }
+
+        public int Number { get; set; }
+
+        public string Process()
+        {
+            if (Number % 3 == 0 && Number % 5 == 0)
+            {
+                return "== FizzBuzz ==";
+            }
+            else if (Number % 3 == 0)
+            {
+                return "== Fizz ==";
+            }
+            else if (Number % 5 == 0)
+            {
+                return "== Buzz ==";
+            }
+            else
+            {
+                return Number.ToString();
+            }
+        }
+    }
+}
---
+++ GraFizzBuzz/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraFizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PROGRAM FizzBuzz\n");
+            Console.WriteLine("Naciśnij w dowolnym momencie 't', aby zakończyć.\n\n");
+            do
+            {
+                Console.Write("\nWpisz liczbę: ");
+                try
+                {
+                    int number = GetIntInputOrExit();
+                    if (number % 3 == 0)
+                    {
+
+                    }
+                    else if (number % 5)
+                    {
+                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Spróbuj ponownie!");
+                    continue;
+                }
+            } while (true);
+
+
+        }
+
+        private static int GetIntInputOrExit()
+        {
+            var value = Console.ReadLine();
+            if(value.ToLower() == "t")
+            {
+                Environment.Exit(0);
+            }
+
+            if (!int.TryParse(value, out int number))
+            {
+                throw new Exception("Podana wartość jest nieprawidłowa.");
+            }
+            return number;
+        }
+
+
+    }
+}
